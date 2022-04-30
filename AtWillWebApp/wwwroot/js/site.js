a392d733e39e9d62474cbbb66e8144bf37822ac6
Masa-86 <mm-supla@nifty.com>
2022-04-30 13:51:57 +0900

    プロジェクト ファイルを追加します。

---
+++ AtWillWebApp/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function addTableRow(tableId, inputId) {
+    const inputValue = document.getElementById(inputId);
+
+    let table = document.getElementById(tableId);
+    let row = table.insertRow(-1);
+    let cell = row.insertCell(-1);
+
+    cell.innerHTML = inputValue.value;
+}
